82180efbf2416a4343f24e8280a140869bf4a963
Alisson-Menanteau <124281137+Alisson-Menanteau@users.noreply.github.com>
2024-01-09 21:37:26 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,3 +1,3 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=ent_test;port=3306;charset=utf8mb4', 'root', '');
+$db = new PDO('mysql:host=localhost;dbname=ent_test;port=3306;charset=utf8mb4', 'root', 'root');
 ?>
