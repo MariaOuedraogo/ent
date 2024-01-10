d9da0a86169f6b0ee4e952977fc3477c749d7ff8
Maria Ouedraogo <maria.ouedraogo.pro@gmail.com>
2024-01-10 09:18:01 +0100

    :tada

--- connexion.php
+++ connexion.php
@@ -1,3 +1,21 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=ent_test;port=3306;charset=utf8mb4', 'root', 'root');
+$host = 'localhost';
+$dbname = 'ent_test';
+$port = '3306';
+$charset = 'utf8mb4';
+$username = 'root';
+$password = '';
+
+if (!empty($password)) {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=$charset", $username, $password);
+    // Reste du code pour traiter la connexion à la base de données avec le mot de passe non vide
+} else {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=$charset", $username, '');
+    // Reste du code pour traiter la connexion à la base de données avec le mot de passe vide (essaie avec 'root')
+}
+?>
+
+
+<?php
+// $db = new PDO('mysql:host=localhost;dbname=ent_test;port=3306;charset=utf8mb4', 'root', '');
 ?>
--- eleve_abs_detail.php
+++ eleve_abs_detail.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualiser les Absences</title>
-    <link rel="stylesheet" href="eleve_abs_index.css">
+    <link rel="stylesheet" href="eleve_abs_detail.css">
 
     <!-- Ajoutez vos liens CSS ici -->
 </head>
